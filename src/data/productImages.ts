4e7afbfc5e5b64139fff920a09b6b4ab9ef17b44
dc0dr47-ops <dc0dr47@gmail.com>
2026-08-17 11:10:57 +0500

    Fix broken Apple product image URLs

--- src/data/productImages.ts
+++ src/data/productImages.ts
@@ -1,9 +1,9 @@
 /** Exact product photography overrides for storefront cards. */
 export const PRODUCT_IMAGE_OVERRIDES: Record<string, string> = {
-  'prod-01': 'https://onei.lk/wp-content/uploads/2024/09/66df67c5161271725917125.jpg',
+  'prod-01': 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-16-pro-max-naturaltitanium-select?wid=1200&hei=1200&fmt=png-alpha',
   'prod-02': 'https://www.challenger.sg/cdn/shop/files/4548736132603_MeCzoRiO2u6do3E89JcCKhIyCW9aEBOEu9AhjW4r.jpg?v=1754983817',
-  'prod-03': 'https://fortresselectronics.co.ke/wp-content/uploads/2024/09/MacBook-Pro.webp',
-  'prod-04': 'https://multimedia.bbycastatic.ca/multimedia/products/1500x1500/184/18470/18470943.jpg',
+  'prod-03': 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16-spaceblack-gallery1-202310?wid=1200&hei=1200&fmt=png-alpha',
+  'prod-04': 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-49-titanium-ultra2_SW_COLOR?wid=1000&hei=1000&fmt=jpeg&qlt=90&.v=1692834364399',
   'prod-05': 'https://images.priceoye.pk/samsung-galaxy-s24-ultra-12-256gb-pakistan-priceoye-govw1-500x500.webp',
   'prod-06': 'https://www.firstshop.co.za/cdn/shop/files/90nv00f1-m00220-controllers-1172581414.jpg?v=1749554237&width=1214',
   'prod-07': 'https://cdn1.avstore.ro/qube/get/w739h600/3fd8cc89bfbd859638409fb7a67d59f5',
