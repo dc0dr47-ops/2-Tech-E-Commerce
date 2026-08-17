14b1f877f466e17e2bf4936ad51c164ca1b1fb90
dc0dr47-ops <dc0dr47@gmail.com>
2026-08-17 11:17:31 +0500

    Fix missing Apple Watch Ultra 2 and Sony WH-1000XM4 images

--- src/data/productImages.ts
+++ src/data/productImages.ts
@@ -3,7 +3,7 @@ export const PRODUCT_IMAGE_OVERRIDES: Record<string, string> = {
   'prod-01': 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-16-pro-max-naturaltitanium-select?wid=1200&hei=1200&fmt=png-alpha',
   'prod-02': 'https://www.challenger.sg/cdn/shop/files/4548736132603_MeCzoRiO2u6do3E89JcCKhIyCW9aEBOEu9AhjW4r.jpg?v=1754983817',
   'prod-03': 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16-spaceblack-gallery1-202310?wid=1200&hei=1200&fmt=png-alpha',
-  'prod-04': 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-49-titanium-ultra2_SW_COLOR?wid=1000&hei=1000&fmt=jpeg&qlt=90&.v=1692834364399',
+  'prod-04': 'https://shop-se.cec.com/image/33955/Apple_Watch_Ultra_2_49mm_Black_Titanium_Ocean_Band_Black_PDP_Image_Position_1__WWEN.jpg',
   'prod-05': 'https://images.priceoye.pk/samsung-galaxy-s24-ultra-12-256gb-pakistan-priceoye-govw1-500x500.webp',
   'prod-06': 'https://www.firstshop.co.za/cdn/shop/files/90nv00f1-m00220-controllers-1172581414.jpg?v=1749554237&width=1214',
   'prod-07': 'https://cdn1.avstore.ro/qube/get/w739h600/3fd8cc89bfbd859638409fb7a67d59f5',
@@ -14,7 +14,7 @@ export const PRODUCT_IMAGE_OVERRIDES: Record<string, string> = {
   'prod-12': 'https://im9.cz/iR/short-tail/977017.jpg',
   'prod-13': 'https://ennap.com/cdn/shop/files/airpods-pro-2nd-generation-with-magsafe-charging-case-usbc-airpods-pro-2nd-generation-with-magsafe-charging-case-usbc-ennap-com-5.jpg?v=1697852900&width=1946',
   'prod-14': 'https://fstudio.vtexassets.com/arquivos/ids/946759/61EvdWaI61L._AC_SL1500_.jpg?v=638423883628870000',
-  'prod-15': 'https://www.intelec.co.cr/wp-content/uploads/2024/08/WH-1000XM4B-1000x1000.webp',
+  'prod-15': 'https://i5.walmartimages.com/asr/ab54bcc9-0c1d-468d-8602-10e51c424401.ef3bbe7d0b10e0479a558224cf102e0c.jpeg?odnBg=FFFFFF&odnHeight=768&odnWidth=768',
   'prod-16': 'https://bfasset.costco-static.com/U447IH35/as/mbp9pwvs4q3srq64w4pxtvck/1402425-894__1?auto=webp&format=jpg',
 };
 
